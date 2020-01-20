3f1e490d7799276c06be28323e713f2851d19e56
Black <blackng10.5.1997@gmail.com>
2020-01-20 23:45:06 +0700

    first commit

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NotFound = (props) => {
+  return (
+    <div>
+      <h2>Not Found !!!!!!!!!!!</h2>
+    </div>
+  )
+}
+
+export default NotFound
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+// let's go!
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import './css/style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Router from './components/Router';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+render(<Router />, document.querySelector('#main'))
