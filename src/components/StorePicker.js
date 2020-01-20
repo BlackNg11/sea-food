1fa4f535883ee87a1affa6131d4391d610f3acd6
Black <blackng10.5.1997@gmail.com>
2020-01-21 01:37:48 +0700

    add route feature

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  /*bind this
+  constructor() {
+    super();
+
+    this.goToStore = this.goToStore.bind(this);
+  }*/
+
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    //1.Stop the form submitting
+    e.preventDefault();
+
+    //2.Get the text form the input
+    const storeName = this.myInput.value.value;
+
+    //3.Change the page to /store/input-value
+    this.props.history.push(`/store/${storeName}`);
+
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     );
